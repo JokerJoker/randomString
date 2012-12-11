90afd0c8d53f3f32493fdfbc12b555901a0c1ac5
jokerjoker <joker@jokerjewel.com>
2012-12-11 20:44:16 +0800

    creat

---
+++ randomString.java
@@ -0,0 +1,15 @@
+import java.util.Random; 
+
+public class randomString {
+
+	public static String getRandomString(int length) { 
+	    StringBuffer buffer = new StringBuffer("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"); 
+	    StringBuffer sb = new StringBuffer(); 
+	    Random r = new Random(); 
+	    int range = buffer.length(); 
+	    for (int i = 0; i < length; i ++) { 
+	        sb.append(buffer.charAt(r.nextInt(range))); 
+	    } 
+	    return sb.toString(); 
+	}
+}
